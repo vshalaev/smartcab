6e3ed80e8dd5859bdc1a1e74f92b304e80362d34
vshalaev <vshalaev@iponweb.net>
2016-07-17 13:07:05 +0300

    implement a basic driving agent

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -23,9 +23,10 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        
+        self.state = [inputs, deadline]
+
         # TODO: Select action according to your policy
-        action = None
+        action = random.choice([None, 'forward', 'left', 'right'])
 
         # Execute action and get reward
         reward = self.env.act(self, action)
@@ -41,7 +42,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
