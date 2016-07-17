22b5560004a8f7fbcee15a588fca6950e5e215cf
vshalaev <vshalaev@iponweb.net>
2016-07-17 13:35:26 +0300

    identify and update state

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -23,7 +23,7 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        self.state = [inputs, deadline]
+        self.state = [inputs, deadline, self.next_waypoint]
 
         # TODO: Select action according to your policy
         action = random.choice([None, 'forward', 'left', 'right'])
